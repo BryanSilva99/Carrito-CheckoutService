7ab065eaf17283e89ff7c1da7765f2e1b687ae7c
bryan <ghingher64@gmail.com>
2025-09-05 16:12:32 -0500

    Primer commit , configuracion de proyecto

---
+++ src/main/java/carritocheckout/carritocheckoutservice/CarritoCheckoutServiceApplication.java
@@ -0,0 +1,13 @@
+package carritocheckout.carritocheckoutservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoCheckoutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarritoCheckoutServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/carritocheckout/carritocheckoutservice/CarritoCheckoutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package carritocheckout.carritocheckoutservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoCheckoutServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
